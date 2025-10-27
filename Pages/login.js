4326139b4409c88d6d169eef9b4c7ab28b11c998
gajerabrijesh11 <gajera.brijesh11@gmail.com>
2025-10-27 14:31:43 +0530

    login test

--- Pages/login.js
+++ Pages/login.js
@@ -1,3 +1,4 @@
+import { expect } from '@playwright/test';
 exports.LoginPage = class LoginPage {
 
     constructor(page) {
@@ -14,5 +15,27 @@ exports.LoginPage = class LoginPage {
         await this.username_textbox.fill(username);
         await this.password_textbox.fill(password);
         await this.login_button.click();
+    }
+    async valid_validation() {
+        try {
+            await expect(this.page).toHaveURL('https://practicetestautomation.com/logged-in-successfully/');
+            console.log("Logged in successfully");
+        } catch (error) {
+            console.log("Login failed");
+        }
+        await expect(this.page.locator('.post-title')).toHaveText('Logged In Successfully');
+        try {
+            await expect(this.page.getByRole('link', { name: 'Log out' })).toBeVisible();
+            console.log("Logout link is visible");
+        } catch (error) {
+            console.log("Logout link is not visible");
+        }
+
+    }
+    async Invalid_user_validation() {
+        await expect(this.page.locator('#error')).toHaveText('Your username is invalid!');
+    }
+    async Invalid_password_validation() {
+        await expect(this.page.locator('#error')).toHaveText('Your password is invalid!');
     }
 } 
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,6 +5,7 @@ test('demo login test', async ({ page }) => {
     const Login = new LoginPage(page);
     await Login.goto();
     await Login.login('student', 'Password123');
+    await Login.valid_validation();
 
     /*await page.goto('https://practicetestautomation.com/practice-test-login/');
     await page.getByRole('textbox', { name: 'Username' }).fill('student');
@@ -16,4 +17,18 @@ test('demo login test', async ({ page }) => {
     await expect(page).toHaveURL('https://practicetestautomation.com/practice-test-login/');
 */
 
+});
+
+test('Incorrect user test', async ({ page }) => {
+    const Login = new LoginPage(page);
+    await Login.goto();
+    await Login.login('student1', 'Password123');
+    await Login.Invalid_user_validation();
+});
+
+test('Incorrect password test', async ({ page }) => {
+    const Login = new LoginPage(page);
+    await Login.goto();
+    await Login.login('student', 'Password1234');
+    await Login.Invalid_password_validation();
 });
