85e92acd338b119185ef470ce764fd6e8d7f9a79
gajerabrijesh11 <gajera.brijesh11@gmail.com>
2025-11-03 14:58:11 +0530

    Add record_webtable

---
+++ Pages/webtable.js
@@ -0,0 +1,31 @@
+import { expect } from "@playwright/test";
+exports.WebtablePage = class WebtablePage {
+    constructor(page) {
+        this.page = page;
+        this.firstname_textbox = page.getByRole('textbox', { name: 'First Name' })
+        this.lastname_textbox = page.getByRole('textbox', { name: 'Last Name' })
+        this.email_textbox = page.getByRole('textbox', { name: 'name@example.com' })
+        this.age_textbox = page.getByRole('textbox', { name: 'Age' })
+        this.salary_textbox = page.getByRole('textbox', { name: 'Salary' })
+        this.department_textbox = page.getByRole('textbox', { name: 'Department' })
+        this.login_button = page.getByRole('button', { name: 'Submit' })
+    }
+    async goto() {
+        await this.page.goto('https://demoqa.com/');
+    }
+    async navigate_to_webtable() {
+        await this.page.locator('div').filter({ hasText: 'Elements' }).nth(5).click();
+        await this.page.getByRole('listitem').filter({ hasText: 'Web Tables' }).click();
+    }
+    async add_user(firstname, lastname, email, age, salary, department) {
+        await this.page.getByRole('button', { name: 'Add' }).click();
+        await this.firstname_textbox.fill(firstname);
+        await this.lastname_textbox.fill(lastname);
+        await this.email_textbox.fill(email);
+        await this.age_textbox.fill(age);
+        await this.salary_textbox.fill(salary);
+        await this.department_textbox.fill(department);
+        await this.login_button.click();
+    }
+
+}
--- tests/webtable.spec.js
+++ tests/webtable.spec.js
@@ -1,24 +1,9 @@
 import { test, expect } from '@playwright/test';
+import { WebtablePage } from '../Pages/webtable.js';
 
-test('test', async ({ page }) => {
-  await page.goto('https://demoqa.com/');
-  await page.locator('div').filter({ hasText: 'Elements' }).nth(5).click();
-  await page.getByRole('listitem').filter({ hasText: 'Web Tables' }).click();
-  await page.getByRole('button', { name: 'Add' }).click();
-  await page.getByRole('textbox', { name: 'First Name' }).click();
-  await page.getByRole('textbox', { name: 'First Name' }).fill('test');
-  await page.getByRole('textbox', { name: 'Last Name' }).click();
-  await page.getByRole('textbox', { name: 'Last Name' }).fill('test');
-  await page.getByRole('textbox', { name: 'name@example.com' }).click();
-  await page.getByRole('textbox', { name: 'name@example.com' }).fill('test@gmail.com');
-  await page.getByRole('textbox', { name: 'Age' }).click();
-  await page.getByRole('textbox', { name: 'Age' }).fill('30');
-  await page.getByRole('textbox', { name: 'Salary' }).click();
-  await page.getByRole('textbox', { name: 'Salary' }).fill('300000');
-  await page.getByRole('textbox', { name: 'Department' }).click();
-  await page.getByRole('textbox', { name: 'Department' }).fill('it');
-  await page.getByRole('button', { name: 'Submit' }).click();
-  await page.getByRole('textbox', { name: 'Type to search' }).click();
-  await page.getByRole('textbox', { name: 'Type to search' }).fill('test');
-  await page.getByRole('gridcell', { name: 'test' }).first().click();
+test('Add record', async ({ page }) => {
+  const webtablePage = new WebtablePage(page);
+  await webtablePage.goto();
+  await webtablePage.navigate_to_webtable();
+  await webtablePage.add_user('test', 'test', 'test@gmail.com', '30', '300000', 'it');
 });
