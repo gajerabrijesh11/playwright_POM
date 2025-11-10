18b5c637a0746517dae9719b7f18e9e3e8672c0e
gajerabrijesh11 <gajera.brijesh11@gmail.com>
2025-11-10 17:34:21 +0530

    allure

--- playwright.config.js
+++ playwright.config.js
@@ -24,12 +24,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html', { open: 'never' }],
-    ['allure-playwright', {
-      resultdir: 'allure-results',
-      detail: true,
-      suiteTitle: true,
-    }]
+    ['list'],
+    ['allure-playwright', { outputFolder: 'allure-results' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
